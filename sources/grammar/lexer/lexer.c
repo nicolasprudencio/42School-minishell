7fb31c824340a734236249eca80e4b49029632ba
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-12-04 21:30:22 -0300

    terminal definition without strdup

--- sources/grammar/lexer/lexer.c
+++ sources/grammar/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:39:37 by nicolas           #+#    #+#             */
-/*   Updated: 2023/12/04 21:23:55 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/12/04 21:30:09 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ t_token *lex_core(char *line, t_dictionary *dict)
 		token = (t_token *)mem_calloc(1, sizeof(t_token));
 		if (!token)
 			return (NULL);
-		token->token_type = str_dup("terminal");
+		token->token_type = "terminal";
 		token->value = str_dup(dict->terminals[g_index]);
 		fp_printf("%s | ", token->token_type);
 		fp_printf("%s\n", token->value);
