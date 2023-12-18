5cfe9cf8fb3db16e719da6c0af4a77f9bf32a077
Fauna Polaris <max.maxpinheiro@gmail.com>
2023-12-18 11:39:00 -0300

    new lex_core function

--- sources/grammar/lexer/lexer.c
+++ sources/grammar/lexer/lexer.c
@@ -13,7 +13,7 @@
 #include "libseas.h"
 
 int g_index;
-
+/*
 static int	check_terminal(t_token **tokens, char *line, t_dictionary *dict)
 {	
 	int	term;
@@ -53,11 +53,7 @@ static int	check_special(t_token **tokens, char *line, t_dictionary *dict)
 	}
 	return (FALSE_INDEX);
 }
-
-int	has_word(char *str)
-{
-	if ()
-}
+*/
 
 void	next_word(char *line)
 {
@@ -67,27 +63,133 @@ void	next_word(char *line)
 	while (line[++i])                                                                                   
 }
 
+static int	st_add_terminal(t_dictionary *dict, t_token **tokens, char *line)
+{
+	int	hold;
+
+	hold = is_terminal(&line[i]);
+	if (hold != FALSE_INDEX)
+	{
+		token_push_last(&tokens,
+				token_new(dict->terminals[hold]), "<TERMINAL>");
+		return (str_len(dict->terminals[hold]));
+	}
+	return (FALSE_INDEX);
+}
+
+static int	st_add_flags(t_dictionary *dict, t_token **tokens, char *line)
+{
+	int		hold;
+	int		i;
+	char	*content;
+
+	hold = -1;
+	if (line[++hold] == '-')
+	{
+		while (is_alpha(line[++hold]))
+			;
+		content = (char *)mem_calloc(hold, sizeof(char));
+		i = -1;
+		while (++i < hold)
+			content[i] = line[i]
+		token_push_last(tokens, token_new(content, "<FLAG>"));
+		free(content);
+		return (hold);
+	}
+	return (FALSE_INDEX);
+}
+
+static int	st_add_string(t_dictionary *dict, t_token **tokens, char *line)
+{
+	int		hold;
+	int		i
+	char	*content;
+
+	hold = -1;
+	if (line[++hold] == '"')
+	{
+		if (!str_find_char(line, '"'))
+			return (-2);
+		while (line[++hold] != '"')
+			;
+	}
+	else if (line[hold] == ''')
+	{
+		if (!str_find_char(line, '''))
+			return (-2);
+		while (line[++hold] != '''
+				;
+	}
+	else
+	{
+		while (!is_space(line[++hold]))
+			;
+	}
+	i = -1;
+	content = (char *)mem_calloc(hold, sizeof(char));
+	while (++i < hold)
+		content[i] = line[i];
+	token_push_last(tokens, token_new(content, "<STRING>"));
+	free(content);
+	return (hold);
+}
+
+static int	st_add_special(t_dictionary *dict, t_token **tokens, char *line)
+{
+	if (line[0] == '<')
+	{
+		if (line[1] == '<')
+			token_push_last(tokens, token_new("<<", "<SPECIAL>"));
+		else
+			token_push_last(tokens, token_new("<", "<SPECIAL>"));
+	}
+	else if (line[0] == '>')
+	{
+		if (line[1] == '>')
+			token_push_last(tokens, token_new(">>", "<SPECIAL>"));
+		else
+			token_push_last(tokens, token_new(">", "<SPECIAL>"));
+	}
+}
+
 t_token	*lex_core(char *line, t_dictionary *dict)
 {
 	int	i;
+	int	hold;
 	t_token	*tokens;
 
 	i = -1;
 	tokens = NULL;
-	while (next_word(&line[i]))
+	while (line[i])
 	{
-		st_find_terms(&line[i], &i, &tokens);
-		st_find_flags(&line[i], &i, &tokens);
-		st_find_strig(&line[i], &i, &tokens);
-		st_find_spcls(&line[i], &i, &tokens);
-		if (line[i] == '|')
+		if (is_terminal(dict, &line[i]))
 		{
-			// add to tokens;
+			hold = st_add_terminal(dict, &tokens, &line[i]);
+			i += hold;
+		}
+		else if (is_flag(&line[i]))
+		{
+			hold = st_add_flag(dict, &tokens, &line[i]);
+			i += hold;
+		}
+		else if (is_special(&line[i]))
+		{
+			hold = st_add_special(&line[i]);
+			i += hold;
+		}
+		else if (line[i] == '|')
+		{
+			token_push(NULL, "<PIPE>");
 			i++;
+		}
+		else
+		{
+			hold = st_add_string_string(dict, &tokens, &line[i]);
+			i += hold;
 		}
 	}
 }
-
+/*
 t_token *lex_core(char *line, t_dictionary *dict)
 {
 	int		i;
@@ -124,7 +226,7 @@ t_token *lex_core(char *line, t_dictionary *dict)
 	tokens_print_list(tokens);
 	return (tokens);
 }
-
+*/
 void	lex_token_free(t_token **tokens)
 {
 	t_token	*aux;
