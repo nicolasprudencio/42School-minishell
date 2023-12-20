e805cfb5db3ba2b393543173b9c26c53f8eb5151
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-12-20 18:03:56 -0300

    com_upto update

--- sources/grammar/lexer/lex_what_is.c
+++ sources/grammar/lexer/lex_what_is.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 11:55:51 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/12/14 18:25:08 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/12/20 17:56:15 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,6 @@ int	exists_token(char *line, char **list)
 	return (FALSE_INDEX);
 }
 */
-static int	str_comp_upto(char *str1, char *str2, char end)
-{
-	int	i;
-
-	i = -1;
-	while (str1[++i] && str1[i] != end)
-	{
-		if (!str2[i] && str1[i] && str1[i] != end)
-			return (-1);
-		if (str1[i] != str2[i])
-			return (1);
-	}
-	return (0);
-}
 
 int	is_terminal(t_dictionary *dict, char *line)
 {
