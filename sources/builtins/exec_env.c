3953ff6243d39fbd09137d39a9bde49d4b71ba3d
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-01-11 20:30:50 -0300

    envs

--- sources/builtins/exec_env.c
+++ sources/builtins/exec_env.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 15:49:27 by nicolas           #+#    #+#             */
-/*   Updated: 2024/01/10 16:28:47 by nprudenc         ###   ########.fr       */
+/*   Updated: 2024/01/11 20:09:03 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	exec_env(t_env_lst *lst, int fd)
 	i = -1;
 	aux = lst;
 	while (aux)
+	{
 		if (str_find_char(aux->value, '=', 0))
-		{
 			put_endl(aux->value, fd);
-			aux = aux->next;
-		}
+		aux = aux->next;
+	}
 }
--- sources/builtins/exec_export.c
+++ sources/builtins/exec_export.c
@@ -6,11 +6,45 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:14:33 by nprudenc          #+#    #+#             */
-/*   Updated: 2024/01/11 17:58:19 by nprudenc         ###   ########.fr       */
+/*   Updated: 2024/01/11 20:29:17 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libseas.h"
+
+static int comp_until(char *str1, char *str2, int c)
+{
+	int	i;
+
+	i = -1;
+	while (str1[++i] && str2[i] && str2[i] != c && str1[i] == str2[i])
+		;
+	if (str1[i] == c && str2[i] == c)
+		return (TRUE);
+	return (FALSE);	
+}
+
+static int	st_to_replace(t_env_lst	**lst, char *var)
+{
+	t_env_lst *aux;
+	t_env_lst *temp;
+
+	aux = *lst;
+	temp = *lst;
+	while (aux)
+	{	
+		if (comp_until(aux->value, var, '=') == 1)
+		{
+			free(aux->value);
+			aux->value = str_dup(var);
+			// lst_del_one(aux);
+			// lst_add_back(lst, lst_new(var));
+			return (TRUE);
+		}
+		aux = aux->next;
+	}
+	return (FALSE);
+}
 
 int	exec_export(t_env_lst *lst, char *var, int fd)
 {
@@ -18,8 +52,11 @@ int	exec_export(t_env_lst *lst, char *var, int fd)
 
 	aux = lst;
 	if (var)
-	{
-		lst_add_back(&lst, lst_new(var));
+	{	
+		if (st_to_replace(&lst, var))
+			return (TRUE);
+		else
+			lst_add_back(&lst, lst_new(var));
 		return (TRUE);
 	}
 	while (aux)
