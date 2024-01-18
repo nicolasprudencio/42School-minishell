0ac02d8fe525e1941ffcfc0307690c7eca735bce
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-01-18 17:37:08 -0300

    heredoc update

--- utilities/env_expand_variables.c
+++ utilities/env_expand_variables.c
@@ -6,165 +6,11 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 17:05:24 by nprudenc          #+#    #+#             */
-/*   Updated: 2024/01/18 12:11:14 by nprudenc         ###   ########.fr       */
+/*   Updated: 2024/01/18 17:03:19 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libseas.h"
-
-// static int	chars_count(char *str, char c, char c2)
-// {
-// 	int	i;
-// 	int	counter;
-
-// 	i = -1;
-// 	counter = 0;
-// 	while (str[++i])
-// 	{
-// 		if (str[i] == c || str[i] == c2)
-// 			counter++;
-// 		if ((str[i] == c && str[i + 1] == c2) || (str[i] == c2 && str[i + 1] == c))
-// 			counter--;
-// 	}
-// 	return (counter);
-// }
-
-// static char	**arr_define(char *str, char **arr, char c, char c2)
-// {	
-// 	int	i;
-// 	int	index_len;
-// 	char *pointer;
-
-// 	i = -1;
-// 	pointer = str;
-// 	index_len = 0;
-// 	while (str[++i])
-// 	{
-// 		if (str[i] == c)
-// 		{
-// 			while (pointer && *pointer == c)
-// 				pointer++;
-// 			arr[index_len] = str_copy_upto(pointer, c);
-// 			index_len++;
-// 			pointer = str_find_char(pointer, c, 0);
-// 		}
-// 		if (str[i] == c2)
-// 		{
-// 			while (pointer && *pointer == c2)
-// 				pointer++;
-// 			arr[index_len] = str_copy_upto(pointer, c2);
-// 			pointer = str_find_char(pointer, c2, 0);
-// 		}
-// 	}
-// 	return (arr);
-// }
-
-// int	fp_is_count(char *str, int (*is)(int))
-// {
-// 	int	i;
-// 	int	output;
-
-// 	if (!str)
-// 		return (0);
-// 	output = 0;
-// 	i = -1;
-// 	while (str[++i])
-// 	{
-// 		if (is(str[i]))
-// 			output++;
-// 	}
-// 	return (output);
-// }
-
-
-
-// static char	**str_split_is(char *str, int (*is)(int))
-// {
-// 	char	**output;
-// 	char		*p;
-// 	int			grid_size;
-// 	int			i;
-
-// 	if (!str)
-// 		return (NULL);
-// 	grid_size = fp_is_count(str, is) + 1;
-// 	if (is(str[0]))
-// 		grid_size--;
-// 	if (is(str[str_len(str) - 1]))
-// 		grid_size--;
-// 	output = (char **)mem_calloc(grid_size + 1, sizeof(char *));
-// 	if (!output)
-// 		return (NULL);
-// 	p = (char *)str;
-// 	i = -1;
-// 	while (++i < grid_size)
-// 	{
-// 		while (p && is(str[i]))
-// 			p++;
-// 		output[i] = str_copy_upto(p, c);
-// 		p = str_find_char(p, c, 0);
-// 	}
-// 	return (output);
-// }
-
-// static int	change_var_value(char **line, t_lst *lst)
-// {	
-// 	t_lst *aux;
-// 	int		checker;
-// 	int		i;
-
-// 	i = -1;
-// 	aux = lst;
-// 	while (line[++i])
-// 	{	
-// 		checker = str_len_until(line[i], '$');
-// 		if (checker != FALSE_INDEX && str_len(line[i]) > 1)
-// 			while (aux)
-// 			{	
-// 				if (str_comp_until(&line[i][checker + 1], aux->value, 1) == TRUE)
-// 				{
-// 					free(line[i]);
-// 					line[i] = str_dup(str_find_char(aux->value, '=', 0));
-// 					return (TRUE);
-// 				}	
-// 				aux = aux->next;
-// 			}
-// 	}
-// 	return (FALSE);
-// }
-
-// static char *str_from_array(char **arr)
-// {
-// 	int	i;
-// 	char *str;
-
-// 	i = -1;
-// 	while (arr[++i])
-// 		str = str_join(str, arr[i], 1);
-// 	// free(arr);
-// 	return (str);
-// }
-
-// char	*env_expand_variables(char	*line, t_lst *lst)
-// {
-// 	char		**str_arr;
-// 	char		*output;
-// 	int			i;
-
-// 	i = -1;
-// 	str_arr = NULL;
-// 	if (str_len_until(line, '$') != FALSE_INDEX)
-// 	{
-// 		str_arr = str_split_bytwo(line, ' ', '\n');
-// 		while (str_arr[++i])
-// 			printf("%s\n", str_arr[i]);
-// 		change_var_value(str_arr, lst);	
-// 		output = str_from_array(str_arr);
-// 	}
-// 	if (str_arr)
-// 		return (output);
-// 	return (NULL);
-// }
 
 char	*check_variable(char *line)
 {
@@ -180,15 +26,7 @@ char	*check_variable(char *line)
 			if (str_len_until(&line[i + 1], ' ') != FALSE_INDEX)
 				var = str_ndup(&line[i + 1], str_len_until(&line[i + 1], ' '));
 			else if (str_len_until(&line[i + 1], '\n') != FALSE_INDEX)
-				var = str_ndup(&line[i + 1], str_len_until(&line[i + 1], '\n'));
-			else if (str_len_until(&line[i + 1], '\t') != FALSE_INDEX)
-				var = str_ndup(&line[i + 1], str_len_until(&line[i + 1], '\t'));
-			else if (str_len_until(&line[i + 1], '\r') != FALSE_INDEX)
-				var = str_ndup(&line[i + 1], str_len_until(&line[i + 1], '\r'));
-			else if (str_len_until(&line[i + 1], '\v') != FALSE_INDEX)
-				var = str_ndup(&line[i + 1], str_len_until(&line[i + 1], '\v'));
-			else if (str_len_until(&line[i + 1], '\f') != FALSE_INDEX)
-				var = str_ndup(&line[i + 1], str_len_until(&line[i + 1], '\f'));	
+				var = str_ndup(&line[i + 1], str_len_until(&line[i + 1], '\\'));	
 			else
 				var = str_ndup(&line[i + 1], str_len_until(&line[i + 1], '\0'));
 		}
@@ -210,7 +48,7 @@ char	*env_expand_variables(char *line, t_lst *lst)
 	{
 		while (aux)
 		{
-			if (str_comp_until(aux->value, var, '=') == TRUE)
+			if (str_comp_until(var, aux->value, '\\') || str_comp_until(aux->value, var, '='))
 			{	
 				free(var);
 				return (str_find_char(aux->value, '=', 1));
