cca3e15ddd12c3dbef45db4b88fe522eb8f23f6b
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-01-18 18:38:01 -0300

    heredoc update

--- utilities/str_comp_until.c
+++ utilities/str_comp_until.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 16:42:18 by nprudenc          #+#    #+#             */
-/*   Updated: 2024/01/18 16:47:34 by nprudenc         ###   ########.fr       */
+/*   Updated: 2024/01/18 18:37:26 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,3 @@ int str_comp_until(char *str1, char *str2, int c)
 		return (TRUE);
 	return (FALSE);	
 }
-
-// int str_comp_until(char *str1, char *str2, int c, int (*is)(int))
-// {
-// 	int	i;
-
-// 	i = -1;
-// 	if (!str1 || !str2)
-// 		return (FALSE);
-// 	while (str1[++i] && str2[i] && str1[i] != c)
-// 		if (str1[i] != str2[i])
-// 			return (FALSE);
-// 	if ((str1[i] == c && str2[i] == '\0') ||
-// 			(str1[i] == c && str2[i] == c) ||
-// 			is(str2[i]))
-// 		return (TRUE);
-// 	return (FALSE);	
-// }
