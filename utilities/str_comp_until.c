babe84725a54188c4e54337970f54e6024f3ba13
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-01-18 17:36:53 -0300

    heredoc update

--- utilities/str_comp_until.c
+++ utilities/str_comp_until.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 16:42:18 by nprudenc          #+#    #+#             */
-/*   Updated: 2024/01/18 11:43:17 by nprudenc         ###   ########.fr       */
+/*   Updated: 2024/01/18 16:47:34 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ int str_comp_until(char *str1, char *str2, int c)
 	while (str1[++i] && str2[i] && str1[i] != c)
 		if (str1[i] != str2[i])
 			return (FALSE);
-	if ((str1[i] == c && str2[i] == '\0') || (str1[i] == c && str2[i] == c) ||
-			(str1[i] == c && is_space(str2[i])))
+	if ((str1[i] == c && str2[i] == '\0') ||
+			(str1[i] == c && str2[i] == c) ||
+				(str1[i] == c && str2[i] == '='))
 		return (TRUE);
 	return (FALSE);	
 }
